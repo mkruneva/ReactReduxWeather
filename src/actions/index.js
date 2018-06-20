660278aa546c35c55ec900738072ede5e88f6813
mkruneva <marta.kruneva@gmail.com>
2018-06-20 10:13:13 +0100

    axios ajax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import { API_KEY } from '../const';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FEATCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    const request = axios.get(url);
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     }
 } 
