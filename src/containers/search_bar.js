e6346fefa30f050f9141f1d020e24784954250d3
mkruneva <marta.kruneva@gmail.com>
2018-06-26 07:01:53 +0100

    search input controlled by state, using bind

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''}
+    }
+
+    onInputChange(e) {
+        console.log(e.target.value);
+    }
+
     render () {
         return (
             <form className="input-group">
-                <input />
+                <input
+                    placeholder="5 day forectast in your chosen  city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
             </form>
         )
     }
