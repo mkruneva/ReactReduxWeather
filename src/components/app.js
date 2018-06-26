b3da097eec7437813c620aa8b31bccc6121c9b66
mkruneva <marta.kruneva@gmail.com>
2018-06-26 06:34:01 +0100

    search bar

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,11 @@ import SearchBar from '../containers/search_bar'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter
+      <div>
          <SearchBar />
+         <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">Submit</button> 
+         </span>
       </div>
      
     );
