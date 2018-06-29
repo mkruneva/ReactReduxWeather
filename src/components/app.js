3d19a7de8ed680f2bf7e883a96709cd942f7bce2
mkruneva <marta.kruneva@gmail.com>
2018-06-29 10:13:13 +0100

    weather list component

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/search_bar'
+import WeatherLst from '../containers/weather_list'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-         <SearchBar />
+        <SearchBar />
+        <WeatherLst />
       </div>
      
     );
---
+++ src/containers/weather_list.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class WeatherLst extends Component {
+    render () {
+        return (
+            <div>WeatherLst</div>
+        );
+    }
+}
