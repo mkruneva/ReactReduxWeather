571050a554ce306604d1b1a71351912649aaea6c
mkruneva <marta.kruneva@gmail.com>
2018-06-30 22:11:00 +0100

    sparkline charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { connect }from 'react-redux';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 // import connect, define mapStatetoPros, pull data from props taken from state to container
 
 class WeatherLst extends Component {
     renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(one => one.main.temp);
+        const humidities = cityData.list.map(one => one.main.humidity);
+        const pressures = cityData.list.map(one => one.main.pressure);
+
         return (
-            <tr key={cityData.city.name}>
-                <td>{cityData.city.name}</td>
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Sparklines height={120} height={180} data={temps}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
             </tr>
         )
     }
